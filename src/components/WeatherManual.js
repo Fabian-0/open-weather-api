615e50d5b3fe7c54944bffb4ce790a085d94bacf
Fabian <fabianjuarezgaliote@gmail.com>
2021-05-31 17:48:07 -0500

    weather app v0.1.4

--- src/components/WeatherManual.js
+++ src/components/WeatherManual.js
@@ -11,7 +11,7 @@ function WeatherManual () {
 
 	useEffect(() => {
 		// Call Api that get the ip
-		fetch("http://cors-anywhere.herokuapp.com/https://api.myip.com",  {headers: {'Access-Control-Allow-Origin': '*'}}).then(res => res.json())
+		fetch("https://cors-anywhere.herokuapp.com/https://api.myip.com",  {headers: {'Access-Control-Allow-Origin': '*'}}).then(res => res.json())
 		.then(res => {
 			// Call api to obtain weather data
 			const { ip } = res;
